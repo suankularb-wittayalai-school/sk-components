86e7f85c5aa2b1266e62215779007aaf856834b7
Jimmy_Tempest <smartwattana@gmail.com>
2022-01-29 21:08:56 +0700

    feat: add rollup for building

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import { terser } from "rollup-plugin-terser";
+import scss from 'rollup-plugin-scss'
+import pkg from './package.json';
+
+export default {
+    input: 'src/js/index.js',
+    plugins: [
+        terser(),
+        scss({
+            output: 'dist/suankularb-components.min.css',
+            outputStyle: "compressed"
+        }),
+    ],
+    output: [
+        {
+            name: 'listree',
+            file: pkg.browser,
+            format: 'umd',
+        },
+        { 
+            file: pkg.module,
+            format: 'es' 
+        },         
+    ],
+};
