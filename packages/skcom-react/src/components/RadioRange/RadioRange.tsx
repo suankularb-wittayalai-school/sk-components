26bc2539c8dcbaaf726d2987ddbd5e31bc6f5b6f
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-20 01:05:07 +0700

    feat: Radio Range

--- packages/skcom-react/src/components/RadioRange/RadioRange.tsx
+++ packages/skcom-react/src/components/RadioRange/RadioRange.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 
 // Helpers
+import { range } from "../../utils/helpers/array";
 import { classNames } from "../../utils/helpers/elements";
 
 // Types
@@ -10,6 +11,8 @@ import { SKComponent } from "../../utils/types";
 export interface RadioRangeProps extends SKComponent {
   name: string;
   steps?: number;
+  minMsg: string | JSX.Element;
+  maxMsg: string | JSX.Element;
   value?: any;
   defaultValue?: any;
   onChange: (newValue: any) => void;
@@ -26,6 +29,8 @@ export interface RadioRangeProps extends SKComponent {
 const RadioRange = ({
   name,
   steps,
+  minMsg,
+  maxMsg,
   value,
   defaultValue,
   onChange,
@@ -35,7 +40,28 @@ const RadioRange = ({
   const [selected, setSelected] = useState<any>(defaultValue || null);
   useEffect(() => onChange(selected), [selected]);
 
-  return <>{/* TODO: Radio Range */}</>;
+  return (
+    <div className={classNames(["radio-range", className])} style={style}>
+      <label htmlFor={[name, 1].join("-")}>{minMsg}</label>
+      <div className="radio-range__buttons">
+        {range(steps || 5).map((step) => {
+          const stepName = [name, step + 1].join("-");
+          return (
+            <input
+              id={stepName}
+              name={name}
+              value={stepName}
+              type="radio"
+              onChange={() =>
+                value ? onChange(step + 1) : setSelected(step + 1)
+              }
+            />
+          );
+        })}
+      </div>
+      <label htmlFor={[name, steps || 5].join("-")}>{maxMsg}</label>
+    </div>
+  );
 };
 
 export default RadioRange;
