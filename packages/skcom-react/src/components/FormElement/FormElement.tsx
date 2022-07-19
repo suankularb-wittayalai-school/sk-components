ba59d99de61d1577f6332c16ca7a4d14ceaecd16
Siravit Phokeed <siravitsasa@gmail.com>
2022-07-20 00:08:10 +0700

    feat: Form Element helper-error msgs

--- packages/skcom-react/src/components/FormElement/FormElement.tsx
+++ packages/skcom-react/src/components/FormElement/FormElement.tsx
@@ -7,17 +7,30 @@ import { SKComponent } from "../../utils/types";
 
 export interface FormElementProps extends SKComponent {
   label: string;
+  helperMsg?: string;
+  errorMsg?: string;
   children: ReactNode;
 }
 
+/**
+ * A wrapper for miscellaneous inputs
+ * @param label A short description of the input, shows above the input
+ * @param helperMsg A message displayed below the input, usually guides or provides an example of what to enter in
+ * @param useAutoMsg Tells the user what’s wrong with the input
+ * @param children Checklist, Radio Group, and Range
+ */
 const FormElement = ({
   label,
+  helperMsg,
+  errorMsg,
   children,
   className,
   style,
 }: FormElementProps): JSX.Element => (
   <div className={classNames(["form-element", className])} style={style}>
     <div className="form-element__label">{label}</div>
+    {helperMsg && <div className="form-element__helper">{helperMsg}</div>}
+    {errorMsg && <div className="form-element__error">{errorMsg}</div>}
     {children}
   </div>
 );
