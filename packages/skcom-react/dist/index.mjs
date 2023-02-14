733dd45f77aa8728a0d898d12a00845c0e3ccad2
Siravit Phokeed <siravitsasa@gmail.com>
2023-02-14 22:19:49 +0700

    feat: link between SKCom Styles and ReSKCom setup

--- packages/skcom-react/dist/index.mjs
+++ packages/skcom-react/dist/index.mjs
@@ -1,3 +1,29 @@
+// #style-inject:#style-inject
+function styleInject(css, { insertAt } = {}) {
+  if (!css || typeof document === "undefined")
+    return;
+  const head = document.head || document.getElementsByTagName("head")[0];
+  const style = document.createElement("style");
+  style.type = "text/css";
+  if (insertAt === "top") {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+// ../skcom-css/dist/css/components/button.css
+styleInject(".button {\n  all: unset;\n  font-family: var(--font-display);\n  background-color: #00658f;\n  color: #ffffff;\n}\n");
+
 // src/Button/index.tsx
 import { jsx } from "react/jsx-runtime";
 function Button(props) {
