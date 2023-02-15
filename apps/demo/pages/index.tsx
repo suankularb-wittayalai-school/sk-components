5569629b97d93677aacd0d37517143c8788847b7
Siravit Phokeed <siravitsasa@gmail.com>
2023-02-15 21:40:07 +0700

    feat: text Button

--- apps/demo/pages/index.tsx
+++ apps/demo/pages/index.tsx
@@ -2,14 +2,23 @@ import { Button } from "@suankularb-components/react";
 
 export default function Home() {
   return (
-    <main className="p-4 items-start flex flex-col gap-2">
+    <main className="p-4 items-start flex flex-row gap-2 flex-wrap">
       <Button appearance="filled">Button</Button>
       <Button appearance="filled" dangerous>
         Button
       </Button>
       <Button appearance="tonal">Button</Button>
+      <Button appearance="tonal" dangerous>
+        Button
+      </Button>
       <Button appearance="outlined">Button</Button>
+      <Button appearance="outlined" dangerous>
+        Button
+      </Button>
       <Button appearance="text">Button</Button>
+      <Button appearance="text" dangerous>
+        Button
+      </Button>
     </main>
   );
 }
