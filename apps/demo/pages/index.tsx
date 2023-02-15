4bd49ab7292715667e11d8aa7991715a66d99007
Siravit Phokeed <siravitsasa@gmail.com>
2023-02-15 21:25:45 +0700

    feat: Tailwind setup in demo

--- apps/demo/pages/index.tsx
+++ apps/demo/pages/index.tsx
@@ -2,9 +2,14 @@ import { Button } from "@suankularb-components/react";
 
 export default function Home() {
   return (
-    <main>
-      <p>TODO กำลังสร้าง</p>
-      <Button>Button</Button>
+    <main className="p-4 items-start flex flex-col gap-2">
+      <Button appearance="filled">Button</Button>
+      <Button appearance="filled" dangerous>
+        Button
+      </Button>
+      <Button appearance="tonal">Button</Button>
+      <Button appearance="outlined">Button</Button>
+      <Button appearance="text">Button</Button>
     </main>
   );
 }
