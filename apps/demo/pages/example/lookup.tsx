657ef1677cc882cc7caadda6099123538ad93d4f
Siravit Phokeed <siravitsasa@gmail.com>
2023-03-08 03:11:46 +0700

    feat: detail side in demo

--- apps/demo/pages/example/lookup.tsx
+++ apps/demo/pages/example/lookup.tsx
@@ -8,6 +8,7 @@ import {
   Card,
   CardHeader,
   ChipSet,
+  Columns,
   ContentLayout,
   Header,
   MaterialIcon,
@@ -23,9 +24,14 @@ const LookupPage: CustomPage = () => (
     <Head>
       <title>Split Layout demo - SK Components</title>
     </Head>
+
     <SplitLayout ratio="list-detail">
+      {/* List side */}
       <aside className="flex flex-col gap-6">
+        {/* Search */}
         <div className="bg-surface-3 h-14 rounded-full" />
+
+        {/* Search results */}
         <div className="flex flex-col gap-2">
           <Card
             appearance="filled"
@@ -67,6 +73,8 @@ const LookupPage: CustomPage = () => (
           </Card>
         </div>
       </aside>
+
+      {/* Details side */}
       <main>
         <Card appearance="outlined" className="h-full overflow-y-auto">
           <div className="bg-surface-2 flex flex-col gap-6 px-5 py-4 md:flex-row">
@@ -91,9 +99,49 @@ const LookupPage: CustomPage = () => (
               </ChipSet>
             </div>
           </div>
+
+          {/* Details content */}
           <ContentLayout>
             <Section>
               <Header level={3}>Contacts</Header>
+              <Columns columns={2}>
+                <Card appearance="outlined" direction="row" stateLayerEffect>
+                  <CardHeader
+                    avatar={<Avatar>FB</Avatar>}
+                    title="Siravit Phokeed"
+                    subtitle="Facebook"
+                  />
+                </Card>
+                <Card appearance="outlined" direction="row" stateLayerEffect>
+                  <CardHeader
+                    avatar={<Avatar>LN</Avatar>}
+                    title="model-siravit-phokeed"
+                    subtitle="Line"
+                  />
+                </Card>
+                <Card appearance="outlined" direction="row" stateLayerEffect>
+                  <CardHeader
+                    avatar={
+                      <Avatar>
+                        <MaterialIcon icon="email" />
+                      </Avatar>
+                    }
+                    title="siravit.pho@student.sk.ac.th"
+                    subtitle="Facebook"
+                  />
+                </Card>
+                <Card appearance="outlined" direction="row" stateLayerEffect>
+                  <CardHeader
+                    avatar={
+                      <Avatar>
+                        <MaterialIcon icon="phone" />
+                      </Avatar>
+                    }
+                    title="012 345 6789"
+                    subtitle="Telephone"
+                  />
+                </Card>
+              </Columns>
             </Section>
           </ContentLayout>
         </Card>
