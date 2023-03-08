5bd2b92707279bcdbf24e1d861a6641341d33033
Siravit Phokeed <siravitsasa@gmail.com>
2023-03-08 21:41:26 +0700

    feat: quick actions in mobile details Split Layout

--- apps/demo/pages/example/lookup/details.tsx
+++ apps/demo/pages/example/lookup/details.tsx
@@ -1,5 +1,6 @@
 // External libraries
 import Head from "next/head";
+import { FC, useState } from "react";
 
 // SK Components
 import {
@@ -9,10 +10,32 @@ import {
 } from "@suankularb-components/react";
 
 // Internal components
+import ShareDialog from "@/components/example/lookup/ShareDialog";
 import StudentDetails from "@/components/example/lookup/StudentDetails";
 
 // Utilities
 import { CustomPage } from "@/utils/types";
+
+const StudentQuickActions: FC = () => {
+  const [showShare, setShowShare] = useState<boolean>(false);
+
+  return (
+    <ChipSet>
+      <AssistChip icon={<MaterialIcon icon="chat" />}>Chat with</AssistChip>
+      <AssistChip icon={<MaterialIcon icon="groups" />}>See class</AssistChip>
+      <AssistChip icon={<MaterialIcon icon="dashboard" />}>
+        See schedule
+      </AssistChip>
+      <AssistChip
+        icon={<MaterialIcon icon="share" />}
+        onClick={() => setShowShare(true)}
+      >
+        Share
+      </AssistChip>
+      <ShareDialog open={showShare} onClose={() => setShowShare(false)} />
+    </ChipSet>
+  );
+};
 
 const LookupDetailsPage: CustomPage = () => (
   <>
@@ -26,16 +49,7 @@ const LookupDetailsPage: CustomPage = () => (
 LookupDetailsPage.pageHeader = {
   title: "Siravit Phokeed",
   parentURL: "/example/lookup",
-  children: (
-    <ChipSet>
-      <AssistChip icon={<MaterialIcon icon="chat" />}>Chat with</AssistChip>
-      <AssistChip icon={<MaterialIcon icon="groups" />}>See class</AssistChip>
-      <AssistChip icon={<MaterialIcon icon="dashboard" />}>
-        See schedule
-      </AssistChip>
-      <AssistChip icon={<MaterialIcon icon="share" />}>Share</AssistChip>
-    </ChipSet>
-  ),
+  children: <StudentQuickActions />,
 };
 
 export default LookupDetailsPage;
