cbe288677d1bac1e1750ebad8b405317265a99a1
Siravit Phokeed <siravitsasa@gmail.com>
2023-03-08 15:54:27 +0700

    refactor: split Lookup example into comps

--- apps/demo/pages/example/lookup.tsx
+++ apps/demo/pages/example/lookup.tsx
@@ -1,6 +1,7 @@
 // External libraries
 import Head from "next/head";
 import Image from "next/image";
+import { FC } from "react";
 
 // SK Components
 import {
@@ -20,6 +21,164 @@ import {
 // Utilities
 import { CustomPage } from "@/utils/types";
 
+// List side
+
+const ListSide: FC = () => (
+  <aside className="flex flex-col gap-6">
+    {/* Search */}
+    <div className="bg-surface-3 h-14 rounded-full" />
+
+    {/* Search results */}
+    <div className="flex flex-col gap-2">
+      <Card
+        appearance="filled"
+        direction="row"
+        stateLayerEffect
+        className="!border-0 !bg-transparent"
+      >
+        <CardHeader
+          avatar={<Avatar>SP</Avatar>}
+          title="Sirapop Prateeppavameta"
+          subtitle="M.505"
+        />
+      </Card>
+      <Card
+        appearance="filled"
+        direction="row"
+        stateLayerEffect
+        className="!bg-primary-container"
+      >
+        <CardHeader
+          avatar={<Avatar className="!bg-primary !text-on-primary">SP</Avatar>}
+          title="Siravit Phokeed"
+          subtitle="M.504"
+        />
+      </Card>
+      <Card
+        appearance="filled"
+        direction="row"
+        stateLayerEffect
+        className="!border-0 !bg-transparent"
+      >
+        <CardHeader
+          avatar={<Avatar>SS</Avatar>}
+          title="Sirawish Sukkee"
+          subtitle="M.504"
+        />
+      </Card>
+    </div>
+  </aside>
+);
+
+// Details side
+
+const StudentDetailsHeader: FC = () => (
+  <div className="bg-surface-2 flex flex-col gap-6 px-5 py-4 md:flex-row">
+    <div>
+      <Avatar className="!h-14 !w-14" />
+    </div>
+    <div className="flex grow flex-col gap-2">
+      <h2 className="skc-display-small">Siravit Phokeed</h2>
+      <ChipSet>
+        <AssistChip icon={<MaterialIcon icon="chat" />}>Chat with</AssistChip>
+        <AssistChip icon={<MaterialIcon icon="groups" />}>See class</AssistChip>
+        <AssistChip icon={<MaterialIcon icon="dashboard" />}>
+          See schedule
+        </AssistChip>
+        <AssistChip icon={<MaterialIcon icon="share" />}>Share</AssistChip>
+      </ChipSet>
+    </div>
+  </div>
+);
+
+/**
+ * A contact Card.
+ * 
+ * @param type The medium in which you can contact this person, i.e. Facebook, e-mail, etc. 
+ * @param label The text label on this contact.
+ * @param href The link to this contact.
+ */
+const ContactCard: FC<{
+  type: "facebook" | "line" | "email" | "tel";
+  label: string;
+  href: string;
+}> = ({ type, label, href }) => {
+  const avatarMap = {
+    facebook: (
+      <Image src="/images/social/facebook.svg" width={40} height={40} alt="" />
+    ),
+    line: <Image src="/images/social/line.svg" width={40} height={40} alt="" />,
+    email: <MaterialIcon icon="email" />,
+    tel: <MaterialIcon icon="phone" />,
+  };
+
+  const subtitleMap = {
+    facebook: "Facebook",
+    line: "Line",
+    email: "E-mail",
+    tel: "Telephone",
+  };
+
+  return (
+    <Card appearance="outlined" direction="row" stateLayerEffect href={href}>
+      <CardHeader
+        avatar={<Avatar>{avatarMap[type]}</Avatar>}
+        title={label}
+        subtitle={subtitleMap[type]}
+      />
+    </Card>
+  );
+};
+
+const StudentDetails: FC = () => (
+  <Card appearance="outlined" className="h-full overflow-y-auto">
+    <StudentDetailsHeader />
+
+    {/* Details content */}
+    <ContentLayout>
+      {/* Contacts section */}
+      <Section>
+        <Header level={3}>Contacts</Header>
+
+        {/* Not free notice */}
+        <div
+          className="text-secondary flex flex-row items-center gap-2 px-2
+          font-medium"
+        >
+          <MaterialIcon icon="info" />
+          <p>According to their schedule, Siravit is not free right now.</p>
+        </div>
+
+        <Columns columns={2}>
+          {/* Facebook */}
+          <ContactCard
+            type="facebook"
+            label="Siravit Phokeed"
+            href="https://www.facebook.com/SKTechDev" // :)
+          />
+
+          {/* Line */}
+          <ContactCard
+            type="line"
+            label="model-siravit-phokeed"
+            href="https://line.me/ti/p/EXAMPLE1234"
+          />
+
+          {/* E-mail */}
+          <ContactCard
+            type="email"
+            label="siravit.pho@student.sk.ac.th"
+            href="mailto:siravit.pho@student.sk.ac.th"
+          />
+
+          {/* Telephone */}
+          <ContactCard type="tel" label="012 345 6789" href="tel:0123456789" />
+        </Columns>
+      </Section>
+    </ContentLayout>
+  </Card>
+);
+
 const LookupPage: CustomPage = () => (
   <>
     <Head>
@@ -27,183 +186,9 @@ const LookupPage: CustomPage = () => (
     </Head>
 
     <SplitLayout ratio="list-detail">
-      {/* List side */}
-      <aside className="flex flex-col gap-6">
-        {/* Search */}
-        <div className="bg-surface-3 h-14 rounded-full" />
-
-        {/* Search results */}
-        <div className="flex flex-col gap-2">
-          <Card
-            appearance="filled"
-            direction="row"
-            stateLayerEffect
-            className="!border-0 !bg-transparent"
-          >
-            <CardHeader
-              avatar={<Avatar>SP</Avatar>}
-              title="Sirapop Prateeppavameta"
-              subtitle="M.505"
-            />
-          </Card>
-          <Card
-            appearance="filled"
-            direction="row"
-            stateLayerEffect
-            className="!bg-primary-container"
-          >
-            <CardHeader
-              avatar={
-                <Avatar className="!bg-primary !text-on-primary">SP</Avatar>
-              }
-              title="Siravit Phokeed"
-              subtitle="M.504"
-            />
-          </Card>
-          <Card
-            appearance="filled"
-            direction="row"
-            stateLayerEffect
-            className="!border-0 !bg-transparent"
-          >
-            <CardHeader
-              avatar={<Avatar>SP</Avatar>}
-              title="Sirawish Sukkee"
-              subtitle="M.504"
-            />
-          </Card>
-        </div>
-      </aside>
-
-      {/* Details side */}
+      <ListSide />
       <main>
-        <Card appearance="outlined" className="h-full overflow-y-auto">
-          <div className="bg-surface-2 flex flex-col gap-6 px-5 py-4 md:flex-row">
-            <div>
-              <Avatar className="!h-14 !w-14" />
-            </div>
-            <div className="flex grow flex-col gap-2">
-              <h2 className="skc-display-small">Siravit Phokeed</h2>
-              <ChipSet>
-                <AssistChip icon={<MaterialIcon icon="chat" />}>
-                  Chat with
-                </AssistChip>
-                <AssistChip icon={<MaterialIcon icon="groups" />}>
-                  See class
-                </AssistChip>
-                <AssistChip icon={<MaterialIcon icon="dashboard" />}>
-                  See schedule
-                </AssistChip>
-                <AssistChip icon={<MaterialIcon icon="share" />}>
-                  Share
-                </AssistChip>
-              </ChipSet>
-            </div>
-          </div>
-
-          {/* Details content */}
-          <ContentLayout>
-            {/* Contacts section */}
-            <Section>
-              <Header level={3}>Contacts</Header>
-
-              {/* Not free notice */}
-              <div
-                className="text-secondary flex flex-row items-center gap-2 px-2
-                  font-medium"
-              >
-                <MaterialIcon icon="info" />
-                <p>
-                  According to their schedule, Siravit is not free right now.
-                </p>
-              </div>
-
-              <Columns columns={2}>
-                {/* Facebook */}
-                <Card
-                  appearance="outlined"
-                  direction="row"
-                  stateLayerEffect
-                  href="https://www.facebook.com/SKTechDev" // :)
-                >
-                  <CardHeader
-                    avatar={
-                      <Avatar>
-                        <Image
-                          src="/images/social/facebook.svg"
-                          width={40}
-                          height={40}
-                          alt=""
-                        />
-                      </Avatar>
-                    }
-                    title="Siravit Phokeed"
-                    subtitle="Facebook"
-                  />
-                </Card>
-
-                {/* Line */}
-                <Card
-                  appearance="outlined"
-                  direction="row"
-                  stateLayerEffect
-                  href="https://line.me/ti/p/EXAMPLE1234"
-                >
-                  <CardHeader
-                    avatar={
-                      <Avatar>
-                        <Image
-                          src="/images/social/line.svg"
-                          width={40}
-                          height={40}
-                          alt=""
-                        />
-                      </Avatar>
-                    }
-                    title="model-siravit-phokeed"
-                    subtitle="Line"
-                  />
-                </Card>
-
-                {/* E-mail */}
-                <Card
-                  appearance="outlined"
-                  direction="row"
-                  stateLayerEffect
-                  href="mailto:siravit.pho@student.sk.ac.th"
-                >
-                  <CardHeader
-                    avatar={
-                      <Avatar>
-                        <MaterialIcon icon="email" />
-                      </Avatar>
-                    }
-                    title="siravit.pho@student.sk.ac.th"
-                    subtitle="E-mail"
-                  />
-                </Card>
-
-                {/* Telephone */}
-                <Card
-                  appearance="outlined"
-                  direction="row"
-                  stateLayerEffect
-                  href="tel:0123456789"
-                >
-                  <CardHeader
-                    avatar={
-                      <Avatar>
-                        <MaterialIcon icon="phone" />
-                      </Avatar>
-                    }
-                    title="012 345 6789"
-                    subtitle="Telephone"
-                  />
-                </Card>
-              </Columns>
-            </Section>
-          </ContentLayout>
-        </Card>
+        <StudentDetails />
       </main>
     </SplitLayout>
   </>
