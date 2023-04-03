3b1b149d5e89b8e35bdcb3a2364323b94c4c58cb
Jimmy-Tempest <smartwattana@gmail.com>
2023-04-03 09:24:37 +0700

    feat: build each component individually

--- packages/skcom-react/tsup.config.ts
+++ packages/skcom-react/tsup.config.ts
@@ -1,5 +1,21 @@
-import { defineConfig } from "tsup";
+import { Options } from "tsup";
 
-export default defineConfig({
+const env = process.env.NODE_ENV;
+
+export const tsup: Options = {
+  splitting: true,
+  clean: true, // clean up the dist folder
+  dts: true, // generate dts files
+  format: ["cjs", "esm"], // generate cjs and esm files
+  minify: env === "production",
+  bundle: env === "production",
+  skipNodeModulesBundle: true,
+  entryPoints: ["src/index.ts"],
+  watch: env === "development",
+  target: "es2020",
+  outDir: "dist",
+  entry: ["src/**/*.ts", "src/**/*.tsx"], //include all files under src
   injectStyle: true,
-});
+  external: ["react"],
+};
+
