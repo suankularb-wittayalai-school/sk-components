847b95bb24b055c02a501f4603205783d5280376
Jimmy-Tempest <smartwattana@gmail.com>
2023-04-03 10:07:38 +0700

    fix: tsup dont include external module

--- packages/skcom-react/tsup.config.ts
+++ packages/skcom-react/tsup.config.ts
@@ -1,17 +1,14 @@
 import { Options } from "tsup";
 
-const env = process.env.NODE_ENV;
-
 export const tsup: Options = {
   splitting: true,
   clean: true, // clean up the dist folder
   dts: true, // generate dts files
   format: ["cjs", "esm"], // generate cjs and esm files
-  minify: env === "production",
-  bundle: env === "production",
-  skipNodeModulesBundle: true,
+  minify: true,
+  bundle: true,
+  // skipNodeModulesBundle: true,
   entryPoints: ["src/index.ts"],
-  watch: env === "development",
   target: "es2020",
   outDir: "dist",
   entry: ["src/**/*.ts", "src/**/*.tsx"], //include all files under src
