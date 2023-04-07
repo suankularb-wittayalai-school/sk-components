104d960a66d83b03e16bdb788ed2dc08375a6e78
Siravit Phokeed <siravitsasa@gmail.com>
2023-04-07 20:28:27 +0700

    fix: FAB disappear state is not working

--- packages/skcom-react/src/components/FAB/index.tsx
+++ packages/skcom-react/src/components/FAB/index.tsx
@@ -162,35 +162,7 @@ export function FAB({
   const { rippleListeners, rippleControls, rippleStyle } = useRipple(fabRef);
 
   // Scroll direction
-  const { scrollDir } = useScrollDirection();
-  const [canHide, setCanHide] = React.useState<boolean>(false);
-
-  // Default scroll position to the top
-  React.useEffect(() => {
-    if (stateOnScroll) {
-      // Disable hiding on Apple platforms since the rubberbanding effect
-      // causes the hiding behavior to malfunction
-      if (/Mac|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-        setCanHide(false);
-        return;
-      }
-
-      const { innerWidth } = window;
-      setCanHide(innerWidth <= 600);
-
-      const handleResize = () => {
-        const { innerWidth } = window;
-        setCanHide(innerWidth <= 600);
-      };
-
-      window.addEventListener("resize", handleResize);
-
-      // Cleanup
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-  }, []);
+  const scrollDir = useScrollDirection();
 
   const props = {
     "aria-label": alt,
@@ -203,7 +175,7 @@ export function FAB({
     <AnimatePresence initial={false}>
       {
         // Hide the FAB on scroll if `stateOnScroll` set to `disappear`
-        !(stateOnScroll === "disappear" && canHide && scrollDir === "down") && (
+        !(stateOnScroll === "disappear" && scrollDir === "down") && (
           <motion.div
             ref={fabRef}
             initial={{ scale: 0.4, x: 20, y: 20, opacity: 0 }}
