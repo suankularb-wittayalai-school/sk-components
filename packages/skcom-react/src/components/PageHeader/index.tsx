315f57da2b276a2c01edcc1257f226f39d755e0f
Siravit Phokeed <siravitsasa@gmail.com>
2023-08-05 22:46:05 +0700

    feat: new Page Header

--- packages/skcom-react/src/components/PageHeader/index.tsx
+++ packages/skcom-react/src/components/PageHeader/index.tsx
@@ -1,10 +1,10 @@
 // External libraries
-import { AnimatePresence, motion } from "framer-motion";
 import * as React from "react";
 
 // Internal components
 import { Button, ButtonProps } from "../Button";
 import { MaterialIcon } from "../MaterialIcon";
+import { Text } from "../Text";
 
 // Types
 import { SKComponent } from "../../types";
@@ -13,7 +13,6 @@ import { SKComponent } from "../../types";
 import "@suankularb-components/css/dist/css/components/page-header.css";
 
 // Utilities
-import { transition, useAnimationConfig } from "../../utils/animation";
 import { cn } from "../../utils/className";
 
 /**
@@ -21,50 +20,30 @@ import { cn } from "../../utils/className";
  */
 export interface PageHeaderProps extends SKComponent {
   /**
-   * Some additional components inside the Page Header area.
-   *
-   * - Ensure that these components are highly related to the title itself.
-   * - Examples are:
-   *   - A Search field in a lookup page (Search is highly related to “School
-   *     lookup”).
-   *   - A TL;DR section and tags in a news article (a summary of an article
-   *     is highly related to the headline).
-   *   - etc.
-   * - Optional.
-   */
-  children?: React.ReactNode;
-
-  /**
    * The title text: the biggest text on a page and the only within a `<h1>`
    * tag.
    *
    * - Always required.
    */
-  title: string | JSX.Element;
+  children: React.ReactNode;
 
   /**
-   * An icon representative of this page of the application.
-   *
-   * - Incompatible with `children`.
+   * @deprecated Use `children` instead.
+   */
+  title?: never;
+
+  /**
+   * @deprecated
    */
   icon?: JSX.Element;
 
   /**
-   * A description of the Page Header for screen readers, similar to `alt` on
-   * `<img>`.
-   *
-   * - Required if `title` is a JSX Element.
+   * @deprecated
    */
   alt?: string;
 
   /**
-   * A small image of your brand can be put on the Page Header to constantly
-   * remind your users that they are, in fact, in your app.
-   *
-   * - Replaces the home Button icon.
-   * - Not displayed on desktop.
-   * - Ensure the image is simple and undistracting.
-   * - Optional but recommended.
+   * @deprecated
    */
   brand?: JSX.Element;
 
@@ -83,11 +62,17 @@ export interface PageHeaderProps extends SKComponent {
   parentURL?: string;
 
   /**
-   * The link to the home page of this application.
-   *
-   * - Optional.
+   * @deprecated
    */
   homeURL?: string;
+
+  /**
+   * An App Drawer placed on the right edge of the Page Header.
+   *
+   * - Recommended if this app is part of a family of apps.
+   * - Optional.
+   */
+  appDrawer?: JSX.Element;
 
   /**
    * Allows for translation of the accessibility labels.
@@ -125,9 +110,7 @@ export interface PageHeaderProps extends SKComponent {
   onNavToggle: () => any;
 
   /**
-   * Props for the underlying Button component the back Button uses.
-   *
-   * - Optional.
+   * @deprecated
    */
   backAttr?: ButtonProps;
 
@@ -140,36 +123,25 @@ export interface PageHeaderProps extends SKComponent {
 /**
  * There’s exactly one Page Header on every page. It displays the title (in
  * the only `<h1>` on the page), the back Button for navigating up, and
- * additional components very closely related to the title.
+ * the App Drawer.
  *
  * @see {@link https://docs.google.com/document/d/1ks5DrzfC_xLg48EFtZALoVQpJpxhsK2It3GDhAhZCcE/edit?usp=sharing#heading=h.5w06ou3fwzsd SKCom documentation}
  *
- * @param children Some additional components inside the Page Header area.
- * @param title The title text: the biggest text on a page and the only within a `<h1>` tag.
- * @param icon The icon representative of this page.
- * @param alt A description of the Page Header for screen readers, similar to `alt` on `<img>`.
- * @param brand A small image of your brand can be put on the Page Header.
+ * @param children The title text: the biggest text on a page and the only within a `<h1>` tag.
  * @param parentURL The link the back Button navigates to.
- * @param homeURL The link to the home page of this application.
  * @param locale Allows for translation of the accessibility labels.
  * @param buttonElement Change the underlying element from `<a>` to a custom element.
  * @param onBack The function triggered when the back Button is clicked.
  * @param onNavToggle The function called when the user clicks on the navigation Button.
- * @param backAttr Props for the underlying Button component the back Button uses.
  */
 export function PageHeader({
   children,
-  title,
-  icon,
-  alt,
-  brand,
   parentURL,
-  homeURL,
+  appDrawer,
   locale,
   buttonElement,
   onBack,
   onNavToggle,
-  backAttr,
   style,
   className,
 }: PageHeaderProps) {
@@ -206,136 +178,99 @@ export function PageHeader({
     return () => document.removeEventListener("scroll", handleScroll);
   }, []);
 
-  // Animation
-  const { duration, easing } = useAnimationConfig();
-
-  // Components
-
-  /**
-   * A Button that navigates up.
-   */
-  const BackButton: React.FC = () => (
-    <Button
-      appearance="text"
-      icon={<MaterialIcon icon="arrow_backward" />}
-      alt={locale === "th" ? "กลับ" : "Back"}
-      onClick={onBack}
-      href={parentURL}
-      element={buttonElement}
-      disabled={!(onBack || parentURL)}
-    />
-  );
-
-  /**
-   * Icons at the end of a Page Header: Home Button and Navigation Toggle.
-   */
-  const TrailingIcons: React.FC = () => (
-    <div className="skc-page-header__trailing">
-      {homeURL && (
-        // Home Button
-        <Button
-          appearance="text"
-          icon={brand || <MaterialIcon icon="home" />}
-          href={homeURL}
-          element={buttonElement}
-          {...backAttr}
-        />
-      )}
-      {/* Menu toggle */}
-      <Button
-        appearance="text"
-        icon={<MaterialIcon icon="menu" />}
-        onClick={onNavToggle}
-      />
-    </div>
-  );
-
   return (
     <>
-      <motion.header
-        ref={headerRef}
-        layoutId="page-header"
-        transition={transition(duration.short4, easing.standard)}
-        style={style}
-        className={cn(["skc-page-header", className])}
-      >
+      <BackgroundBlobs />
+      <header style={style} className={cn([`skc-page-header`, className])}>
         <div className="skc-page-header__content">
-          {/* Background icon */}
-          <AnimatePresence mode="popLayout" initial={false}>
-            {icon && !children && (
-              <motion.div
-                key={typeof title === "string" ? title : alt}
-                initial={{ opacity: 0, scale: 1.4, y: "-50%" }}
-                animate={{ opacity: 0.08, scale: 1, y: "-50%" }}
-                exit={{ opacity: 0, scale: 0.9 }}
-                transition={transition(
-                  duration.medium4,
-                  easing.standardDecelerate
-                )}
-                className="skc-page-header__icon"
-              >
-                {icon}
-              </motion.div>
-            )}
-          </AnimatePresence>
-
-          <div className="skc-page-header__actions">
-            <BackButton />
-            <TrailingIcons />
-          </div>
-
-          {/* Page title */}
-          <AnimatePresence mode="wait" initial={false}>
-            <motion.h1
-              key={typeof title === "string" ? title : alt}
-              aria-label={alt}
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{
-                opacity: 0,
-                scale: 0.8,
-                transition: transition(
-                  duration.short1,
-                  easing.standardAccelerate
-                ),
-              }}
-              transition={transition(
-                duration.medium2,
-                easing.standardDecelerate
-              )}
-            >
-              {title}
-            </motion.h1>
-          </AnimatePresence>
-
-          {/* Related content */}
-          {children && (
-            <div className="skc-page-header__related">{children}</div>
+          {parentURL || onBack ? (
+            // Back Button
+            <Button
+              appearance="text"
+              icon={<MaterialIcon icon="arrow_backward" />}
+              alt={locale === "th" ? "กลับ" : "Navigate up"}
+              onClick={onBack}
+              href={parentURL}
+              element={buttonElement}
+            />
+          ) : (
+            // Navigation Toggle
+            <Button
+              appearance="text"
+              icon={<MaterialIcon icon="menu" />}
+              alt={locale === "th" ? "เปิดเมนู" : "Open navigation"}
+              onClick={onNavToggle}
+              className="skc-page-header__nav-toggle"
+            />
           )}
-        </div>
-      </motion.header>
 
-      {/* Minimized */}
-      <AnimatePresence>
-        {minimized && (
-          <motion.div
-            aria-hidden
-            initial={{ opacity: 0, scaleY: 0.5, y: "-50%" }}
-            animate={{ opacity: 1, scaleY: 1, y: "0%" }}
-            exit={{ opacity: 0 }}
-            transition={transition(duration.short4, easing.standard)}
-            className="skc-page-header__minimized"
+          {/* Header text */}
+          <Text
+            type="headline-large"
+            element="h1"
+            className="skc-page-header__text"
           >
-            <div className="skc-page-header__minimized-content">
-              <BackButton />
-              <h1>{title}</h1>
-              <TrailingIcons />
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+            {children}
+          </Text>
+
+          {/* App Drawer */}
+          {appDrawer}
+        </div>
+      </header>
     </>
   );
 }
+
+/**
+ * Blurred blobs behind the Page Header. Contains both the mobile version and
+ * the desktop version, with only the appropriate one being shown at a time.
+ */
+const BackgroundBlobs: React.FC = () =>
+  // prettier-ignore
+  <>
+    {/* Desktop version */}
+    <svg
+      viewBox="0 0 1194 263"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="skc-page-header__blobs skc-page-header__blobs--desktop"
+    >
+      <g filter="url(#filter)">
+        <path d="M336.5 -76.75C336.5 -13.7895 113.011 103.75 50.0502 103.75C-12.9103 103.75 -91 -25.7895 -91 -88.75C-91 -151.71 121.54 -326 184.5 -326C247.46 -326 336.5 -139.71 336.5 -76.75Z" className="skc-page-header__blob-secondary"/>
+        <path d="M1154 -90.75C1154 -27.7895 930.511 89.75 867.55 89.75C804.59 89.75 634 -27.7895 634 -90.75C634 -153.71 939.04 -340 1002 -340C1064.96 -340 1154 -153.71 1154 -90.75Z" className="skc-page-header__blob-secondary"/>
+        <path d="M1182.6 140.656C1245.56 140.656 1315.1 141.906 1286.1 7.65555C1257.1 -126.594 982.102 -84.5944 917.101 7.65555C852.101 99.9055 1119.64 140.656 1182.6 140.656Z" className="skc-page-header__blob-primary"/>
+        <path d="M655.25 -16C655.25 46.9605 307.71 70.25 244.75 70.25C181.79 70.25 123 -39.5395 123 -102.5C123 -165.46 238.04 -174 301 -174C363.96 -174 655.25 -78.9605 655.25 -16Z" className="skc-page-header__blob-primary"/>
+      </g>
+      <defs>
+        <filter id="filter" x="-191" y="-440" width="1584" height="680" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="50" result="blur" />
+        </filter>
+      </defs>
+    </svg>
+
+    {/* Mobile version */}
+    <svg
+      viewBox="0 0 393 260"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="skc-page-header__blobs skc-page-header__blobs--mobile"
+    >
+      <g filter="url(#filter)">
+        <path d="M92.215 73.9492C92.215 97.6861 18.9131 142 -1.73719 142C-22.3875 142 -48 93.1619 -48 69.425C-48 45.6881 21.7105 -20.0212 42.3608 -20.0212C63.0111 -20.0212 92.215 50.2123 92.215 73.9492Z" className="skc-page-header__blob-secondary"/>
+        <path d="M390.554 5.97042C390.554 29.7073 317.252 74.0212 296.602 74.0212C275.951 74.0212 220 29.7073 220 5.97042C220 -17.7665 320.049 -88 340.7 -88C361.35 -88 390.554 -17.7665 390.554 5.97042Z" className="skc-page-header__blob-secondary"/>
+        <path d="M395.394 90.436C416.044 90.436 438.853 90.9072 429.341 40.2933C419.829 -10.3207 329.632 5.51383 308.313 40.2933C286.994 75.0727 374.744 90.436 395.394 90.436Z" className="skc-page-header__blob-primary"/>
+        <path d="M219.572 28.0521C219.572 51.789 105.583 60.5695 84.9326 60.5695C64.2823 60.5695 45 19.1774 45 -4.55946C45 -28.2964 82.7316 -31.5159 103.382 -31.5159C124.032 -31.5159 219.572 4.31524 219.572 28.0521Z" className="skc-page-header__blob-primary"/>
+      </g>
+      <defs>
+        <filter id="filter" x="-148" y="-188" width="680" height="430" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="50" result="blur" />
+        </filter>
+      </defs>
+    </svg>
+  </>;
 
 PageHeader.displayName = "PageHeader";
